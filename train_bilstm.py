ede53ca338459a4ce8f1c9591558cae2aeccbb65
Lemuel Horas <lemuelhorasdaniel@gmail.com>
2024-11-20 23:12:13 +0700

    Deleted train file

--- train_bilstm.py
+++
@@ -1,7 +0,0 @@
-from Model import bilstm
-from train import hypertune
-
-bi_lstm = bilstm.BuildBiLSTM
-ss_bilstm_model, ss_bilstm_history, ss_bilstm_pred, ss_bilstm_actual, test_date = hypertune("MYOR.JK", 0.1, 0.1, "2024-11-1", bi_lstm, "SingleShot")
-seq2seq_bilstm_model, seq2seq_bilstm_history, seq2seq_bilstm_pred, seq2seq_bilstm_actual, test_date = hypertune("MYOR.JK", 0.1, 0.1, "2024-11-1", bi_lstm, "Seq2Seq")
-ar_bilstm_model, ar_bilstm_history, ar_bilstm_pred, ar_bilstm_actual, test_date = hypertune("MYOR.JK", 0.1, 0.1, "2024-11-1", bi_lstm, "Autoregressive")
--- train_grulstm.py
+++
@@ -1,7 +0,0 @@
-from Model import gru_lstm
-from train import hypertune
-
-grulstm = gru_lstm.BuildGRULSTM
-ss_grulstm_model, ss_grulstm_history, ss_grulstm_pred, ss_grulstm_actual, test_date = hypertune("MYOR.JK", 0.1, 0.1, "2024-11-1", grulstm, "SingleShot")
-seq2seq_grulstm_model, seq2seq_grulstm_history, seq2seq_grulstm_pred, seq2seq_grulstm_actual, test_date = hypertune("MYOR.JK", 0.1, 0.1, "2024-11-1", grulstm, "Seq2Seq")
-ar_grulstm_model, ar_grulstm_history, ar_grulstm_pred, ar_grulstm_actual, test_date = hypertune("MYOR.JK", 0.1, 0.1, "2024-11-1", grulstm, "Autoregressive")
